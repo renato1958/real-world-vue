964bc9eeee2387f717b44e1a5ced2e68d1b91ec6
renato1958 <felisi.renato@gmail.com>
2021-12-19 13:09:08 +0100

    Corretto errore

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link :to="{name: 'EventList'}">Events</router-link> |
-      <router-link to="{name: 'About'}">About</router-link>
+      <router-link :to="{name: 'About'}">About</router-link>
     </div>
     <router-view />
   </div>
